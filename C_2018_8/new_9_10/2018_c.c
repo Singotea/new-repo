ebee44f9d856082f3f8ca394cf7d0680decddc51
lin thao <646179293@qq.com>
2018-09-10 22:15:07 +0800

    2018年9月10日 22:15:05

---
+++ C_2018_8/new_9_10/2018_c.c
@@ -0,0 +1,12 @@
+// C resource just for personal learning
+// time 2018-9-10 22:06:54
+// author thao lin
+#include <stdio.h>
+int main(void){
+    
+    printf("+=============================+\n");
+    printf("+===== ÄãºÃ  hello world =====+\n");
+    printf("+=============================+\n");
+    
+    // return;
+}
